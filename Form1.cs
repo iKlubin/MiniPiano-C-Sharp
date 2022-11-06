4552905a81e348355c882ae9e539a19606046ea5
Klubin <69307031+iKlubin@users.noreply.github.com>
2022-11-06 19:57:08 +0200

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,322 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.Beep(123, 300);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Console.Beep(130, 300);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Console.Beep(138, 300);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Console.Beep(146, 300);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Console.Beep(155, 300);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Console.Beep(164, 300);
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Console.Beep(174, 300);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Console.Beep(184, 300);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Console.Beep(195, 300);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Console.Beep(207, 300);
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Console.Beep(220, 300);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Console.Beep(233, 300);
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Console.Beep(246, 300);
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            Console.Beep(261, 300);
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            Console.Beep(277, 300);
+        }
+
+        private void button23_Click(object sender, EventArgs e)
+        {
+            Console.Beep(293, 300);
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            Console.Beep(311, 300);
+        }
+
+        private void button24_Click(object sender, EventArgs e)
+        {
+            Console.Beep(329, 300);
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            Console.Beep(349, 300);
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            Console.Beep(369, 300);
+        }
+
+        private void button25_Click(object sender, EventArgs e)
+        {
+            Console.Beep(391, 300);
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            Console.Beep(415, 300);
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            Console.Beep(440, 300);
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Console.Beep(466, 300);
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Console.Beep(493, 300);
+        }
+
+        private void button34_Click(object sender, EventArgs e)
+        {
+            Console.Beep(523, 300);
+        }
+
+        private void button33_Click(object sender, EventArgs e)
+        {
+            Console.Beep(554, 300);
+        }
+
+        private void button35_Click(object sender, EventArgs e)
+        {
+            Console.Beep(587, 300);
+        }
+
+        private void button32_Click(object sender, EventArgs e)
+        {
+            Console.Beep(622, 300);
+        }
+
+        private void button36_Click(object sender, EventArgs e)
+        {
+            Console.Beep(659, 300);
+        }
+
+        private void button31_Click(object sender, EventArgs e)
+        {
+            Console.Beep(698, 300);
+        }
+
+        private void button30_Click(object sender, EventArgs e)
+        {
+            Console.Beep(739, 300);
+        }
+
+        private void button37_Click(object sender, EventArgs e)
+        {
+            Console.Beep(783, 300);
+        }
+
+        private void button28_Click(object sender, EventArgs e)
+        {
+            Console.Beep(830, 300);
+        }
+
+        private void button29_Click(object sender, EventArgs e)
+        {
+            Console.Beep(880, 300);
+        }
+
+        private void button26_Click(object sender, EventArgs e)
+        {
+            Console.Beep(932, 300);
+        }
+
+        private void button27_Click(object sender, EventArgs e)
+        {
+            Console.Beep(987, 300);
+        }
+
+        private void button38_Click(object sender, EventArgs e)
+        {
+            Console.Beep(1046, 300);
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            button15.PerformClick();
+            button15.PerformClick();
+            button15.PerformClick();
+            button35.PerformClick();
+            button34.PerformClick();
+            button15.PerformClick();
+            button15.PerformClick();
+            button24.PerformClick();
+            button15.PerformClick();
+            button35.PerformClick();
+            button34.PerformClick();
+            button15.PerformClick();
+            button15.PerformClick();
+            button15.PerformClick();
+            button35.PerformClick();
+            button34.PerformClick();
+            button15.PerformClick();
+            button15.PerformClick();
+            button24.PerformClick();
+            button15.PerformClick();
+            button24.PerformClick();
+            button24.PerformClick();
+            button15.PerformClick();
+        }
+
+        private void button1_Click_2(object sender, EventArgs e)
+        {
+            button34.PerformClick();
+            button36.PerformClick();
+            button37.PerformClick();
+            button38.PerformClick();
+            button37.PerformClick();
+            button36.PerformClick();
+            button17.PerformClick();
+            button34.PerformClick();
+            button36.PerformClick();
+            button29.PerformClick();
+            button36.PerformClick();
+            button34.PerformClick();
+            button19.PerformClick();
+            button17.PerformClick();
+            button34.PerformClick();
+            button31.PerformClick();
+            button34.PerformClick();
+            button17.PerformClick();
+            button37.PerformClick();
+        }
+
+        private void button39_Click(object sender, EventArgs e)
+        {
+            button37.PerformClick();
+            button37.PerformClick();
+            button37.PerformClick();
+            button26.PerformClick();
+            button32.PerformClick();
+            button35.PerformClick();
+            button37.PerformClick();
+            button37.PerformClick();
+            button37.PerformClick();
+            button26.PerformClick();
+            button35.PerformClick();
+            button34.PerformClick();
+            button37.PerformClick();
+            button37.PerformClick();
+            button37.PerformClick();
+            button37.PerformClick();
+            button37.PerformClick();
+            button28.PerformClick();
+            button28.PerformClick();
+            button28.PerformClick();
+            button28.PerformClick();
+            button37.PerformClick();
+            button37.PerformClick();
+        }
+
+        private void button40_Click(object sender, EventArgs e)
+        {
+            button23.PerformClick();
+            button23.PerformClick();
+            button20.PerformClick();
+            button24.PerformClick();
+            button25.PerformClick();
+            button24.PerformClick();
+            button25.PerformClick();
+            button24.PerformClick();
+            button24.PerformClick();
+            button20.PerformClick();
+            button23.PerformClick();
+            button17.PerformClick();
+            button23.PerformClick();
+            button23.PerformClick();
+            button17.PerformClick();
+        }
+
+        private void button41_Click(object sender, EventArgs e)
+        {
+            button17.PerformClick();
+            button24.PerformClick();
+            button16.PerformClick();
+            button33.PerformClick();
+            button16.PerformClick();
+            button24.PerformClick();
+            button34.PerformClick();
+            button35.PerformClick();
+            button36.PerformClick();
+            button35.PerformClick();
+            button34.PerformClick();
+            button17.PerformClick();
+        }
+    }
+}
